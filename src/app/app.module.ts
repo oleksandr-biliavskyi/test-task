09d39014c90eb47dfefbdfab16b95cc263e93938
Oleksandr Biliavskyi <oleksandr@stellar.aero>
2018-04-26 18:29:07 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import './rxjs-operators';
 import {DnaService} from './state/dna.service';
 import {HttpClientModule} from '@angular/common/http';
 import {DnaFacade} from './state/dna.facade';
 import { DnaItemComponent } from './dna-item/dna-item.component';
 
+import './rxjs-operators';
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/rxjs-operators.ts
+++ src/app/rxjs-operators.ts
@@ -1,26 +1,2 @@
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/takeWhile';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/throttleTime';
-import 'rxjs/add/operator/skip';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/merge';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/concatMap';
-import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/finally';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/pairwise';
-import 'rxjs/add/operator/retry';
-
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/fromPromise';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/observable/forkJoin';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/observable/empty';
